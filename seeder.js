08a3e90003d251eb43dfcfe79698d3423e70d459
phadidi <hadidip@hotmail.com>
2020-09-29 11:07:06 -0700

    Added reviews to seeder

--- seeder.js
+++ seeder.js
@@ -7,6 +7,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/bootcamp');
 const Course = require('./models/course');
 const User = require('./models/user');
+const Review = require('./models/review');
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -26,12 +27,19 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
+
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
-    console.log('bootcamps.json, courses.json, users.json imported'.cyan);
+    await Review.create(reviews);
+    console.log(
+      'bootcamps.json, courses.json, users.json, reviews.json imported'.cyan
+    );
     process.exit();
   } catch (e) {
     console.error(e);
@@ -43,7 +51,8 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
-    console.log('bootcamps, courses, user data cleared'.magenta);
+    await Review.deleteMany();
+    console.log('bootcamps, courses, users, reviews data cleared'.magenta);
     process.exit();
   } catch (e) {
     console.error(e);
